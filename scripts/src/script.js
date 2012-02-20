9b73ec8637e7f04d74225d9baaaa2c613bc1d5af
Matthew Solle <denopi@gmail.com>
2012-02-20 15:37:56 -0500

    worked on javascript, minor html changes

--- scripts/src/script.js
+++ scripts/src/script.js
@@ -1,10 +1,13 @@
 $(document).ready(function(){
+	$("#searchAgain").hide();
 	$("#search").click(function(){
 		$("#search_bar").fadeOut();
-		$("#tweets").fadeIn();
+//		$("#tweets").fadeIn();
+		$("#searchAgain").fadeIn();
 	main();
 	});
 });
+
 
 function main () {
 
@@ -20,12 +23,11 @@ function main () {
 		$("#search_bar").fadeIn();
 		$("#tweets").remove();
 		s.stop();
+		$("#searchAgain").hide();
 	});
-	var term_count = 0;
-	var t_count = 0;
+	
 	var count = 0;
 	var tweetNumber = [] ;
-	var statCount = [] ;
 
 s.register(function(tweet){ 
 
@@ -37,18 +39,19 @@ s.register(function(tweet){
 	var day = dateParse[0];
 	var time = dateParse[4];
 	var color;
+	var t_count = 0;
 	count++;
-
+	var term_count = 0;
+	
 	//5. Alternate the colors or the background of the tweets (create a variable that changes the css class of the tweet based on the order that it's coming in)
 	if (count%2 === 0){
 		color="backgroundColor";  //backgroundColor is defined in the css file
 	} else{
 		color="backgroundColor2"; //backgroundColor2 is defined in the css file
 	}
-
+	
 	var tweetReceived = $("<p class = '"+color+"'>"+profile_image+"&nbsp;"+user_name+"&nbsp;"+day+"&nbsp"+time+"<br />"+tweet.text+"</p>");
-	var tweetStats = $("<p> love =" +term_count+ " </p>");
-
+	
 	    //6. Show a maximum of 10 tweets at a time (remove old tweets from the dom) (easiest way is to use arrays to create a cycle)
 		if (tweetNumber.length >= 10) { //checks number of tweets against array size
 			var p = tweetNumber.shift();  //moves to other end of array
@@ -57,39 +60,34 @@ s.register(function(tweet){
 			});
 		};
 		
-		if (statCount.length >= 1) { //checks number of tweets against array size
-			var d = statCount.shift();  //moves to other end of array
-			d.fadeOut(500, function() {  //fades out element of 500ms
-			d.remove(); //removes it from array
-			});
-		};
-
-
 	tweetNumber.push(tweetReceived); //pushes tweet to array
 	tweetReceived.hide(); //hides from dom
- 
  //3. Make the tweets occur so the most recent are at the top (check jquery documentation)
-	$("#tweets").prepend(tweetReceived); //add it to the DOM, still invisible
-
-
-
+$("#tweets").prepend(tweetReceived); //add it to the DOM, still invisible
 //4. Make the tweets slide down (store temporarily in a jquery object, then apply slidedown)
+	
+	var results = $("<p> " + term_count + "</p>") ;
+	$("#searchedTerms").append(results);
+	t_count++;
+	if(t_count = 1) {
+		$("#searchedTerms").hide();
+		t_count--;
+	}
 
-	tweetReceived.slideDown(); //make it appear by sliding it down
+tweetReceived.slideDown(); //make it appear by sliding it down
 
 //Check to see if tweet has the word love
-if(tweet.text.match(/(^|\s)term($|\s)/)) {
-//if(tweet.text.match(/love/i) || (tweet.text.match(/hate/i))) {	
-//	alert("sup") ;
+//if(tweet.text.match(/(^|\s)"#term"($|\s)/)) {
+if(tweet.text.match(/love/i) || (tweet.text.match(/hate/i))) {
+//	alert(tweet.text) ;
 	term_count = term_count++;
-	statCount = statCount++;
 	}
-	
-	statCount.push(tweetStats);
-	tweetStats.hide();
-	$("#searchedTerms").prepend(tweetStats);
-	tweetStats.slideDown() ;
 });
+
 
 s.start(); //start the spotter   
 }
+
+//$(document).ready(function() {
+//	main();
+//});
